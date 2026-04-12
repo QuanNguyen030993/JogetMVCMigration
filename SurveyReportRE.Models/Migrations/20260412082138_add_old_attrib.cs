0138ed530fc6268a6e10fe7266fa467137aef4b7
Quan Nguyen <nguyen.hongquan030993@gmail.com>
2026-04-12 16:00:11 +0700

    main: update code 20260410

---
+++ SurveyReportRE.Models/Migrations/20260412082138_add_old_attrib.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyReportRE.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_old_attrib : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "type",
+                table: "MailQueue",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "subject",
+                table: "MailQueue",
+                newName: "Subject");
+
+            migrationBuilder.RenameColumn(
+                name: "cc",
+                table: "MailQueue",
+                newName: "CC");
+
+            migrationBuilder.RenameColumn(
+                name: "bcc",
+                table: "MailQueue",
+                newName: "BCC");
+
+            migrationBuilder.RenameColumn(
+                name: "attachments",
+                table: "MailQueue",
+                newName: "Attachments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "is_send",
+                table: "MailQueue",
+                type: "nvarchar(max)",
+                maxLength: 8000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Attachments",
+                table: "MailQueue",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 8000,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FromAccount",
+                table: "MailQueue",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HtmlBody",
+                table: "MailQueue",
+                type: "nvarchar(max)",
+                maxLength: 8000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSend",
+                table: "MailQueue",
+                type: "bit",
+                nullable: true,
+                defaultValueSql: "0");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TextBody",
+                table: "MailQueue",
+                type: "nvarchar(max)",
+                maxLength: 8000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ToEmail",
+                table: "MailQueue",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ToName",
+                table: "MailQueue",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FromAccount",
+                table: "MailQueue");
+
+            migrationBuilder.DropColumn(
+                name: "HtmlBody",
+                table: "MailQueue");
+
+            migrationBuilder.DropColumn(
+                name: "IsSend",
+                table: "MailQueue");
+
+            migrationBuilder.DropColumn(
+                name: "TextBody",
+                table: "MailQueue");
+
+            migrationBuilder.DropColumn(
+                name: "ToEmail",
+                table: "MailQueue");
+
+            migrationBuilder.DropColumn(
+                name: "ToName",
+                table: "MailQueue");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "MailQueue",
+                newName: "type");
+
+            migrationBuilder.RenameColumn(
+                name: "Subject",
+                table: "MailQueue",
+                newName: "subject");
+
+            migrationBuilder.RenameColumn(
+                name: "CC",
+                table: "MailQueue",
+                newName: "cc");
+
+            migrationBuilder.RenameColumn(
+                name: "BCC",
+                table: "MailQueue",
+                newName: "bcc");
+
+            migrationBuilder.RenameColumn(
+                name: "Attachments",
+                table: "MailQueue",
+                newName: "attachments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "is_send",
+                table: "MailQueue",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 8000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "attachments",
+                table: "MailQueue",
+                type: "nvarchar(max)",
+                maxLength: 8000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+        }
+    }
+}
--- SurveyReportRE.Models/Models/Migration/Business/Data/MailQueue.cs
+++ SurveyReportRE.Models/Models/Migration/Business/Data/MailQueue.cs
@@ -7,17 +7,25 @@ namespace SurveyReportRE.Models.Migration.Business.Data
     {
         public string  to_name {get;set;} = "";
         public string  to_email {get;set;} = "";
-        public string  subject {get;set;} = "";
         [MaxLength(8000)]
         public string  text_body {get;set;} = "";
         [MaxLength(8000)]
         public string  html_body {get;set;} = "";
-        public string  cc {get;set;} = "";
-        public string  bcc {get;set;} = "";
         public string  from_account {get;set;} = "";
-        public string  type {get;set;} = "";
         [MaxLength(8000)]
-        public string attachments { get; set; } = "";
         public string  is_send {get;set;} = "";
+        public string ToName { get; set; } = "";
+        public string ToEmail { get; set; } = "";
+        public string Subject { get; set; } = "";
+        [MaxLength(8000)]
+        public string TextBody { get; set; } = "";
+        [MaxLength(8000)]
+        public string HtmlBody { get; set; } = "";
+        public string CC { get; set; } = "";
+        public string BCC { get; set; } = "";
+        public string FromAccount { get; set; } = "";
+        public string Type { get; set; } = "";
+        public string Attachments { get; set; } = "";
+        public bool? IsSend { get; set; } = false;
     }
 }
