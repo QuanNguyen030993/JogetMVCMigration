b0325804827c5d8d53a40a16181cb40069ae2441
quannh6 <quan.nh@tokiomarine.com.vn>
2026-06-17 17:32:25 +0700

    main: Update code 20260617

---
+++ SurveyReportRE.Models/Migrations/20260617103135_update_field.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyReportRE.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_field : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstanceWorkflowId",
+                table: "WorkflowInstanceNode");
+
+            migrationBuilder.DropColumn(
+                name: "QuotationStatus",
+                table: "Quotation");
+
+            migrationBuilder.DropColumn(
+                name: "PolicyIssuanceStatus",
+                table: "PolicyIssuance");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "InstanceWorkflowId",
+                table: "WorkflowInstanceNode",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QuotationStatus",
+                table: "Quotation",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PolicyIssuanceStatus",
+                table: "PolicyIssuance",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+        }
+    }
+}
