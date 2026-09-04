bfcc8fa46b1bbe33a67f7b26f51330c2964e1f97
quannh6 <quan.nh@tokiomarine.com.vn>
2026-09-04 17:14:18 +0700

    main: Update code 20260904

---
+++ SurveyReportRE.Models/Migrations/20260904063644_add_Sub_PI_Detail.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyReportRE.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_Sub_PI_Detail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Endorsment",
+                table: "PolicyIssuanceSubDetails");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TranNo",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Renew",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuotationCode",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PolicyNo",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ClientId",
+                table: "PolicyIssuanceSubDetails",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientName",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endorsement",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "PolicyIssuanceSubDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ClientName",
+                table: "PolicyIssuanceSubDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Endorsement",
+                table: "PolicyIssuanceSubDetails");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TranNo",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Renew",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuotationCode",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PolicyNo",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Endorsment",
+                table: "PolicyIssuanceSubDetails",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+        }
+    }
+}
